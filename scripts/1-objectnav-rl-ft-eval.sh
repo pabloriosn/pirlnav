9d499e99a18d3d5d2eba2c14327af0744e18e4c5
Pablo <pablo.rios@edu.uah.es>
2023-04-13 16:01:31 +0200

    Modify script to run evaluation

--- scripts/1-objectnav-rl-ft-eval.sh
+++ scripts/1-objectnav-rl-ft-eval.sh
@@ -1,44 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=pirlnav
-#SBATCH --gres gpu:1
-#SBATCH --nodes 1
-#SBATCH --cpus-per-task 8
-#SBATCH --ntasks-per-node 1
-#SBATCH --signal=USR1@1000
-#SBATCH --partition=short
-#SBATCH --constraint=a40
-#SBATCH --output=slurm_logs/ddprl-eval-%j.out
-#SBATCH --error=slurm_logs/ddprl-eval-%j.err
-#SBATCH --requeue
-
-source /srv/flash1/rramrakhya6/miniconda3/etc/profile.d/conda.sh
-conda deactivate
-conda activate pirlnav
 
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
-MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
-export MASTER_ADDR
-
 config="configs/experiments/rl_ft_objectnav.yaml"
 
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d/objectnav_hm3d_v1/"
 TENSORBOARD_DIR="tb/objectnav_il_rl_ft/ovrl_resnet50/seed_1/"
-EVAL_CKPT_PATH_DIR=$1
+EVAL_CKPT_PATH_DIR="model/objectnav_rl_ft_hd.ckpt"
 
 mkdir -p $TENSORBOARD_DIR
 set -x
 
 echo "In ObjectNav IL DDP"
-srun python -u -m run \
---exp-config $config \
---run-type eval \
+python -u -m run --exp-config $config --run-type eval \
 TENSORBOARD_DIR $TENSORBOARD_DIR \
 EVAL_CKPT_PATH_DIR $EVAL_CKPT_PATH_DIR \
 NUM_UPDATES 20000 \
-NUM_ENVIRONMENTS 8 \
+NUM_ENVIRONMENTS 1 \
 RL.DDPPO.pretrained False \
 TASK_CONFIG.DATASET.SPLIT "val" \
 TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
