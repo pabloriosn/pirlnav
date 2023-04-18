239fad871c82646cbded6ac524bbad0c4e150e1b
Pablo <pablo.rios@edu.uah.es>
2023-04-18 19:59:24 +0200

    Change environment name

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 git submodule update --init
 
-conda create -n habitat_pirlnav python=3.7 cmake=3.14.0 -y
+conda create -n pirlnav python=3.7 cmake=3.14.0 -y
 
-conda activate habitat_pirlnav
+conda activate pirlnav
 
 # Install torch
 pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
